1362224384e87c7d8dbac5c8ad0b23d65f4eab3c
zeyuan <zzy412619@qq.com>
2020-03-10 08:07:27 +0000

    编辑个人资料

--- routes/web.php
+++ routes/web.php
@@ -37,9 +37,10 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //显示用户个人信息页面
-Route::get('/users/{user}','UsersController@show')->name('users.show');
-//显示编辑个人资料页面
-Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
-//处理edit 页面提交的更改
-Route::patch('/users/{user}','UsersController@update')->name('users.update');
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// //显示编辑个人资料页面
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// //处理edit 页面提交的更改
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
