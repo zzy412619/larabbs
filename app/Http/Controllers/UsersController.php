b5260781aabc348119535438fab07c1a858ce240
zeyuan <zzy412619@qq.com>
2020-03-11 03:28:06 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,13 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+	public function __construct()
+	{
+		//除了以下动作不需要登录就能访问，其他都要登录
+		$this->middleware('auth',[
+			'except' =>['show']
+		]);
+	}
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
@@ -16,11 +23,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+    	$this->authorize('update',$user);
     	return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+    	 $this->authorize('update', $user);
     	// 将用户输入的 姓名，密码等信息赋值给$data,以便对数据更新操作
     	$data = $request->all();
 
