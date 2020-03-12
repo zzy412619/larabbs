25b153fe86675c05897120bdc34d9381b2dc5a36
zeyuan <zzy412619@qq.com>
2020-03-12 07:45:31 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
 	<!-- Styles -->
 	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+	@yield('styles')
 </head>
 <body>
 	<div id="app" class="{{ route_class() }}-page">
@@ -28,5 +29,8 @@
 	
 	</div>
 	<script src="{{ mix('js/app.js') }}"></script>
+
+	@yield('scripts')
+	
 </body>
 </html>
