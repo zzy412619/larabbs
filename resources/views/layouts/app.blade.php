60fcd72fdcbbc84889c889a7b96b0d8f92d4a716
zeyuan <zzy412619@qq.com>
2020-03-12 09:11:35 +0000

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
+	<meta name="description" content="@yield('description','LaraBBS 爱好者社区')" />
 
 	<!-- Styles -->
 	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
@@ -31,6 +32,6 @@
 	<script src="{{ mix('js/app.js') }}"></script>
 
 	@yield('scripts')
-	
+
 </body>
 </html>
