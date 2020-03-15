0af21ef1848d317a9e4688dc124ae07d46373c68
zeyuan <zzy412619@qq.com>
2020-03-15 05:01:04 +0000

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,33 +3,29 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
-    }
     public function saving(Topic $topic)
     {
-    	// 防止xss攻击
-    	$topic->body = clean($topic->body, 'user_topic_body');
-    	// make_excerpt() 是我们自定义的辅助方法，我们需要在 helpers.php 文件中添加：
-    	//生成话题摘录
-    	$topic->excerpt = make_excerpt($topic->body);
-    	
-    	//如slug 字段无内容，即使用翻译器对title进行翻译
-    	 if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        // XSS 过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 生成话题摘录
+        $topic->excerpt = make_excerpt($topic->body);
+    }
+
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
