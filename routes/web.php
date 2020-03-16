3bba39c893ae2fd3626b73a6b6cbbb5d353d7f83
zeyuan <zzy412619@qq.com>
2020-03-16 03:31:34 +0000

    用户可以发表评论

--- routes/web.php
+++ routes/web.php
@@ -53,4 +53,6 @@ Route::resource('categories','CategoriesController',['only'=>['show']]);
 
 //上传图片
 Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 回复相关
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
